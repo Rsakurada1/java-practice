ba51676fbaccb0997fa016e766ab37bd0099380b
Rikito Sakurada <rikito82k@gmail.com>
2024-03-02 15:19:11 +0900

    環境変数の設定完了。コンパイル動作確認完了。

---
+++ Greeting.java
@@ -0,0 +1,13 @@
+//ファイル名の頭文字は大文字
+//クラス名とファイル名は同じ
+
+class greeting {
+    public static void main(String[] args){
+        System.out.println("Good morning!");
+
+        System.out.println("Good afternoon!");
+
+        System.out.println("Good evening!");
+
+    }
+}
